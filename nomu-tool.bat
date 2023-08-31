5716393d135be417780b66f5bd95be97a9873afa
Asyahdan <54228809+asyahdan@users.noreply.github.com>
2023-08-31 18:03:12 +0700

    Add files via upload

---
+++ nomu-tool.bat
@@ -0,0 +1,11 @@
+@echo off
+
+:mainMenu
+echo -------------------------------------
+echo "Nomu th19 mod ^-^"
+echo =====================================
+echo pilih Bit OS
+echo -------------------------------------
+set /p user_input=Masukan IP address PF: 
+echo IP address PF : %user_input% 
+host64.exe %user_input% th19.exe
